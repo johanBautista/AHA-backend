0e6807138f1e912d97842ac573aee45d16abb1f9
Thor Jubera Albo <thor.jubera.albo@gmail.com>
2019-08-07 08:47:41 +0200

    update seed

--- bin/seed.js
+++ bin/seed.js
@@ -5,17 +5,18 @@ const Book = require('../models/Book');
 
 mongoose.connect('mongodb://localhost/miBiblio', { useNewUrlParser: true });
 
-const books = [];
-for (let i = 0; i < 30; i++) {
-  books.push({
-    title: faker.hacker.phrase(),
-    description: faker.lorem.sentences(3),
-    author: faker.name.firstName(),
-    rating: faker.random.number(1, 5),
-  });
-}
-
-Book.insertMany(books)
+const books = Array.from({ length: 40 }, () => ({
+  title: faker.hacker.phrase(),
+  description: faker.lorem.sentences(3),
+  author: faker.name.firstName(),
+  rating: faker.random.number(1, 5),
+}));
+Book.collection
+  .drop()
+  .then(() => {
+    console.log('deleted db');
+    return Book.insertMany(books);
+  })
   .then(() => {
     console.log('inserted fake data');
     mongoose.connection.close();
