5c49ed4b3028c0581274adf76c516c8666484626
johan Bautista <jsbparra@gmail.com>
2019-11-12 20:51:00 +0100

    wip routes event

--- routes/event.js
+++ routes/event.js
@@ -3,7 +3,7 @@ const Event = require('../models/Event');
 
 const router = express.Router();
 
-router.post('/event', async (req, res, next) => {
+router.post('/events', async (req, res, next) => {
   const { title, description, date, location } = req.body;
   // console.log(typeofowner, 'que es esto');
   try {
@@ -17,6 +17,16 @@ router.post('/event', async (req, res, next) => {
   } catch (error) {
     next(error);
   }
+});
+
+router.get('/events', async (req, res, next) => {
+  try {
+    const event = await Event.find();
+    return res.json(event);
+  } catch (error) {
+    next(error);
+  }
+  // req.flash('prueba de lisatado'); PENSAR EN LA FORMA DE HACER NOTIFICACIONES
 });
 
 module.exports = router;
